a6f411720d1fa67d873576848bb7ac4cf136fd7b
DeyvissonRobert <robert.designerx@gmail.com>
2025-07-24 17:19:52 -0300

    Escrevendo a lista de filmes em json

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -80,8 +80,8 @@ public class Titulo implements Comparable<Titulo> {
 
     @Override
     public String toString() {
-        return "nome = '" + nome + '\'' +
+        return "(nome = " + nome  +
                 ", anoDeLancamento = " + anoDeLancamento + "," +
-                "duração = " + duracaoEmMinutos;
+                " duração = " + duracaoEmMinutos + ")";
     }
 }
